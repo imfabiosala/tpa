69ad5b5619031ac66dbd95e1790f35484189f9eb
Fábio Sala <imfabiosala@gmail.com>
2023-01-17 20:37:09 -0300

    [FEAT] Add. toolbar.

--- src/app/layout-page/layout-page.component.ts
+++ src/app/layout-page/layout-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { PoMenuItem } from '@po-ui/ng-components';
+import { PoToolbarProfile } from '@po-ui/ng-components';
+import { PoToolbarAction } from '@po-ui/ng-components';
 
 @Component({
   selector: 'layout-page',
@@ -21,6 +23,25 @@ export class LayoutPageComponent implements OnInit {
       shortLabel: 'Produtos',
       icon: 'po-icon-stock',
       link: 'products'
+    }
+  ];
+
+  profile: PoToolbarProfile = {
+    avatar: 'https://avatars.githubusercontent.com/u/109047244?v=4',
+    subtitle: 'im@fabiosala.dev',
+    title: 'Fábio Sala'
+  };
+
+  profileActions: Array<PoToolbarAction> = [
+    {
+      icon: 'po-icon-user',
+      label: 'Perfil'
+    },
+    {
+      icon: 'po-icon-exit',
+      label: 'Sair',
+      type: 'danger',
+      separator: true
     }
   ];
 
