83bff3e5ada16c3bf097f476ab43c83ac5eda938
Fábio Sala <imfabiosala@gmail.com>
2023-01-17 20:01:50 -0300

    [FEAT] Add. sidebar.

--- src/app/layout-page/layout-page.component.ts
+++ src/app/layout-page/layout-page.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PoMenuItem } from '@po-ui/ng-components';
+
 @Component({
   selector: 'layout-page',
   templateUrl: './layout-page.component.html'
 })
 
 export class LayoutPageComponent implements OnInit {
+
+  menu: Array<PoMenuItem> = [
+    {
+      label: 'Dashboard',
+      shortLabel: 'Dashboard',
+      icon: 'po-icon-chart-columns',
+      link: 'dashboard'
+    }
+  ];
 
   constructor() { }
 
--- src/app/layout-page/layout-page.module.ts
+++ src/app/layout-page/layout-page.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { PoModule } from '@po-ui/ng-components';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
+
 import { LayoutPageComponent } from './layout-page.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 
 @NgModule({
   declarations: [LayoutPageComponent, DashboardComponent],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule, PoModule, PoTemplatesModule]
 })
 
 export class LayoutPageModule { }
